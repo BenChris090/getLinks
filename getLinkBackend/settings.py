dc1124b44bd85a2174ff382bc8a50d3859014c2a
unknown <you@example.com>
2022-08-07 23:11:12 +0100

    first commit

--- getLinkBackend/settings.py
+++ getLinkBackend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rmat@!(q3qa&r&a0vkvd635ubz)t6l=qux328&j^doc4alp+to'
+import secretKey
+SECRET_KEY = secretKey.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
